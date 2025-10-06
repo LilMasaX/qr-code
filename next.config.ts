fe3047c48b1cf599cc79368c3e72bfbf55253bdc
lilmasax <luzu09092005@gmail.com>
2025-10-06 17:12:58 -0500

    disable eslint for deploy only once

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // evita que fallos de lint cancelen `next build`
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
